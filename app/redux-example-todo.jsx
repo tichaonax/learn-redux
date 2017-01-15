a27a439e808b3d5bb4c0c49fc3e915d06b4e2665
Tichaona Hwandaza <tichaona@yahoo.com>
2017-01-15 16:04:42 -0600

    added redux change searchText action

--- app/redux-example-todo.jsx
+++ app/redux-example-todo.jsx
@@ -12,11 +12,31 @@ const stateDefault = {
 var reducer = (state = stateDefault
     , action) => {
 
+    console.log('New action :', action);
 
-    return state;
+    switch (action.type) {
+
+        //use the spread operator to preserve the old state
+        case 'CHANGE_SEARCH_TEXT':
+            return {
+                ...state,
+                searchText: action.searchText
+            }
+            break
+        default:
+            return state;
+            break;
+    }
 }
 
 var store = redux.createStore(reducer);
+
+console.log('currentState', store.getState());
+
+store.dispatch({
+    type: 'CHANGE_SEARCH_TEXT',
+    searchText: '401k'
+});
 
 console.log('currentState', store.getState());
 
